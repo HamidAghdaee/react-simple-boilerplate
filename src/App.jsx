373a709955f42a17de4cb07a1ceef380c2c31240
Hamid Aghdaee <hamid.aghdaee@galvanize.com>
2017-01-22 08:31:45 -0800

    added start

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
-import CountDownTimer from './CountDownTimer.jsx';
+import React, {Component} from 'react';
+import CountDownTimer from './CountDownTimer';
 
-export default () => {
-  return (
-    <div className="app">
-      <CountDownTimer></CountDownTimer>
-    </div>
-  )
+export default class App extends Component {
+  countDownTimerComplete() {
+    alert('time\'s up!');
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <CountDownTimer
+          startTime="7"
+          onTimerDone={this.countDownTimerComplete}>
+          <button>Start Timer</button>
+        </CountDownTimer>
+
+      </div>
+    )
+  }
 };
--- src/CountDownTimer.jsx
+++ src/CountDownTimer.jsx
@@ -1,6 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const CountDownTimer = () => {
-  return <h1>05:00</h1>
+class CountDownTimer extends Component {
+  constructor(props) {
+    super(props);
+    // initialize state to what's passed in
+    this.state = {
+      time: props.startTime
+    };
+  }
+  startTimer() {
+    this.timer = setInterval(() => {
+      const time = this.state.time - 1;
+      this.setState({ time });
+
+      if (time === 0) {
+        this.props.onTimerDone();
+        clearInterval(this.timer);
+      }
+    }, 1000);
+  }
+  render() {
+    const self = this;
+    const childrenWithProps = React.Children.map(this.props.children,
+     (child) => {
+       console.log('inside map', this)
+       return React.cloneElement(child, {
+         onClick: () => this.startTimer()
+       })
+     }
+    );
+    // render value stored in state
+    const startTime = this.state.time;
+    return (
+      <h1>
+        {startTime}
+        {childrenWithProps}
+      </h1>
+    )
+  }
 };
+
 export default CountDownTimer;
